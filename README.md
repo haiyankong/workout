9fd08866bdd260ccfa980db508a472336220e966
haiyankong <80819972+haiyankong@users.noreply.github.com>
2025-05-06 21:22:55 +0800

    recommit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Original repo
+
+- [yihong0618](https://github.com/yihong0618)/[running_page](https://github.com/yihong0618/running_page)
+
+- [ben-29](https://github.com/ben-29)/[workouts_page](https://github.com/ben-29/workouts_page)
+
+# 抄作业
+
+- [也搞了一个展示行走数据的页面 | 椒盐豆豉](https://blog.douchi.space/steps-page/)
---
+++ src/static/site-metadata.ts
@@ -0,0 +1,31 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  keywords: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const data: ISiteMetadataResult = {
+  siteTitle: 'Workout',
+  siteUrl: '/',
+  logo: '',
+  description: 'Workout',
+  keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
+  navLinks: [
+    {
+      name: 'Strava',
+      url: 'https://www.strava.com/athletes/149557983',
+    },
+    {
+      name: 'Repo',
+      url: 'https://github.com/haiyankong/workout',
+    },
+  ],
+};
+
+export default data;
