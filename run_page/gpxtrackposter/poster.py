56def58bbd919a07c59d9549f55bfb7ed09c6b70
haiyankong <80819972+haiyankong@users.noreply.github.com>
2025-08-26 00:54:07 -0400

    TIMEZONE change v1

--- run_page/gpxtrackposter/poster.py
+++ run_page/gpxtrackposter/poster.py
@@ -59,7 +59,7 @@ class Poster:
         self.tracks_drawer = None
         self.trans = None
         self.set_language(None)
-        self.tc_offset = datetime.now(pytz.timezone("Asia/Shanghai")).utcoffset()
+        self.tc_offset = datetime.now(pytz.timezone("America/New_York")).utcoffset()
         self.github_style = "align-firstday"
 
     def set_language(self, language):
