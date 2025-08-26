e481f5e86fbebeee45ed0da0eeb1339b5392bd06
haiyankong <80819972+haiyankong@users.noreply.github.com>
2025-08-26 01:16:02 -0400

    change Mapbox token
    reference: https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,8 @@ const MAPBOX_TOKEN =
     // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
   // Instead, manually add a new token and apply URL restrictions.
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  // now changed 20250826
+  'pk.eyJ1IjoiaGFpeWFua29uZyIsImEiOiJjbWVzM2IzYXMwZGxyMmxwdGtkZDV5azRpIn0.BCaNDxcG7avCJlZVfTq8SA';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
