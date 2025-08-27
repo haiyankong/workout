94dac9bad11f22dce922c9795696f55224acd948
haiyankong <80819972+haiyankong@users.noreply.github.com>
2025-08-26 21:58:11 -0400

    change Mapbox style v2

--- src/components/RunMap/index.tsx
+++ src/components/RunMap/index.tsx
@@ -133,7 +133,7 @@ const RunMap = ({
       {...viewState}
       onMove={onMove}
       style={style}
-      mapStyle="mapbox://styles/mapbox/outdoors-v12"
+      mapStyle="mapbox://styles/mapbox/satellite-v9"
       ref={mapRefCallback}
       mapboxAccessToken={MAPBOX_TOKEN}
     >
